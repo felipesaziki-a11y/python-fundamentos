3185423be342943eaef56948deac7c7b37edb2f6
Felipe <felipesaziki@gmail.com>
2026-07-29 19:31:04 -0300

    adicionado arquivo de try/execept

--- exemplo10.py
+++ exemplo10.py
@@ -1,6 +1,7 @@
 # /try-execept
 def exemplo_sem_tratamento():
     print("Divisão:", 10 / 0)
+    print("A")
     # Lança a exceção ZeroDivisionError: division by zero
 
 
@@ -9,7 +10,9 @@ def exemplo_com_tratamento():
         print("Divisão:", 10 / 0)
     except ZeroDivisionError:
         print("Não é possível dividir um número por 0")
+    print("Tudo normar")
 
 
+# Ponto de entrada da aplicação, deve ter um único da aplicação inteira
 if __name__ == "__main__":
-    exemplo_sem_tratamento()
+    exemplo_com_tratamento()
